77bebd4563c76592dadce6990d8b8cdd7a45ae77
Rexagon <reide740@gmail.com>
2019-07-23 12:03:27 +0300

    Исправил пути

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 
 call clean.bat
 
-mkdir CMakeFiles
-cd CMakeFiles
+mkdir cmakefiles_gcc
+cd cmakefiles_gcc
 
 cmake .. -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=../dist -DCMAKE_BUILD_TYPE=Release
 cmake --build . --target install
