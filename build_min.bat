7c06d13bfad8d951cd64a943a7a1efe439733ad4
Rexagon <reide740@gmail.com>
2019-07-23 11:33:28 +0300

    Добавил скрипты для сборки

--- build_min.bat
+++ build_min.bat
@@ -6,4 +6,5 @@ mkdir CMakeFiles
 cd CMakeFiles
 
 cmake .. -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=../dist -DCMAKE_BUILD_TYPE=Release
-cmake --build . --target install
+cmake --build . --target sitl
+cmake -DCOMPONENT=sitl -P cmake_install.cmake
--- clean.bat
+++ clean.bat
@@ -1,2 +1,4 @@
+rmdir /s /q CMakeFiles 1>NUL 2>NUL
 rmdir /s /q temp 1>NUL 2>NUL
 rmdir /s /q build 1>NUL 2>NUL
+rmdir /s /q dist 1>NUL 2>NUL
