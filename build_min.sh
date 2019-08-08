1ce0f74aa92e046ab2360ba769a95e756ac549be
Rexagon <reide740@gmail.com>
2019-08-08 15:55:21 +0300

    Исправил скрипты для сборки

--- build_min.sh
+++ build_min.sh
@@ -1,9 +1,13 @@
-sh clean.sh
+#!/bin/sh
 
-sh prepare.sh
+cwd=`dirname "$0"`
 
-mkdir -p cmakefiles_gcc/sitl
-cd cmakefiles_gcc/sitl
+sh $cwd/clean.sh
+
+sh $cwd/prepare.sh
+
+mkdir -p $cwd/cmakefiles_gcc/sitl
+cd $cwd/cmakefiles_gcc/sitl
 
 cmake -DCMAKE_TOOLCHAIN_FILE=~/mingw-w64-i686.cmake -DCMAKE_INSTALL_PREFIX=../../dist -DCMAKE_BUILD_TYPE=Release ../../
 cmake --build . --target sitl
--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,14 @@
-sh clean.sh
+#!/bin/sh
+
+cwd=`dirname "$0"`
+
+sh $cwd/clean.sh
 
 git submodule init
 git submodule update
 
-mkdir -p cmakefiles_gcc/3rdparty/Catch2
-cd cmakefiles_gcc/3rdparty/Catch2
+mkdir -p $cwd/cmakefiles_gcc/3rdparty/Catch2
+cd $cwd/cmakefiles_gcc/3rdparty/Catch2
 
 cmake -DCMAKE_TOOLCHAIN_FILE=~/mingw-w64-i686.cmake -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../../3rdparty/Catch2/dist ../../../3rdparty/Catch2
 cmake --build . --target install
