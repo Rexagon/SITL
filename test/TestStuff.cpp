8963bb5ff7bfad32069607c9b99319e6ae95eb0a
Rexagon <reide740@gmail.com>
2019-08-13 12:52:37 +0300

    Добавил тесты для перевода hex в строки и обратно

--- test/TestStuff.cpp
+++ test/TestStuff.cpp
@@ -9,11 +9,15 @@
 
 TEST_CASE("Test converter")
 {
-    REQUIRE(sitl::stuff::convertToHex(static_cast<uint8_t>(0x3F)) == "3F");
-    REQUIRE(sitl::stuff::convertToHex(static_cast<uint16_t>(0x103F)) == "103F");
+    REQUIRE(sitl::stuff::convertToHex(static_cast<uint8_t>(0x3Fu)) == "3F");
+    REQUIRE(sitl::stuff::convertToHex(static_cast<uint16_t>(0x103Fu)) == "103F");
+    REQUIRE(sitl::stuff::convertToHex(static_cast<uint32_t>(0x12A1103Fu)) == "12A1103F");
+    REQUIRE(sitl::stuff::convertToHex(static_cast<uint64_t>(0xDEADBEAF12A1103Fu)) == "DEADBEAF12A1103F");
 
     REQUIRE(sitl::stuff::convertFromHex("3F") == 0x3Fu);
     REQUIRE(sitl::stuff::convertFromHex("103F") == 0x103Fu);
+    REQUIRE(sitl::stuff::convertFromHex("12A1103F") == 0x12A1103Fu);
+    REQUIRE(sitl::stuff::convertFromHex("DEADBEAF12A1103F") == 0xDEADBEAF12A1103Fu);
 }
 
 TEST_CASE("Test string split")
