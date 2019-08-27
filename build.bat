f8927c0e3a2df70b40a13048558ea95651c23aa3
Rexagon <reide740@gmail.com>
2019-08-27 13:41:04 +0300

    Сделал опциональной обёртку для питона

--- build.bat
+++ build.bat
@@ -4,11 +4,18 @@ PUSHD %CD%
 call prepare.bat
 POPD
 
+set withoutBindings=0
+for %%x in (%*) do (
+    if "%%~x" EQU "--without-bindings" set withoutBindings=1
+)
+
+if %withoutBindings% EQU 1 set additionalParameters=%additionalParameters% -DBUILD_PYSITL=OFF
+
 mkdir cmakefiles_gcc
 cd cmakefiles_gcc
 
 mkdir sitl
 cd sitl
 
-cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=../../dist -DCMAKE_BUILD_TYPE=Release ../../
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=../../dist -DCMAKE_BUILD_TYPE=Release %additionalParameters% ../../
 cmake --build . --target install
